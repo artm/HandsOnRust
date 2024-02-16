eef82f9e4c45dfda346a1b178ba9c90358fe441e
Artem Baguinski <abaguinski@gmail.com>
2024-02-16 21:26:16 +0000

    Make Clippy happier

--- InstallingRust/Clippy/src/main.rs
+++ InstallingRust/Clippy/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let MYLIST = [ "One", "Two", "Three" ];
-    for i in 0..3 {
-        println!("{}", MYLIST[i]);
+    let my_list = ["One", "Two", "Three"];
+    for item in my_list {
+        println!("{}", item);
     }
 }
