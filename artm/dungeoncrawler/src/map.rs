ba13db889ee1335150ffb9b5e0988ed669febee3
Artem Baguinski <abaguinski@gmail.com>
2024-02-19 07:16:53 +0000

    Start the empty dungeon

---
+++ artm/dungeoncrawler/src/map.rs
@@ -0,0 +1,39 @@
+use crate::prelude::*;
+
+const NUM_TILES: usize = (SCREEN_WIDTH * SCREEN_HEIGHT) as usize;
+
+#[derive(Copy, Clone, PartialEq)]
+pub enum TileType {
+    Wall,
+    Floor,
+}
+
+pub struct Map {
+    pub tiles: Vec<TileType>,
+}
+
+impl Map {
+    pub fn new() -> Self {
+        Self {
+            tiles: vec![TileType::Floor; NUM_TILES],
+        }
+    }
+
+    pub fn map_idx(&self, x: i32, y: i32) -> usize {
+        (x + y * SCREEN_WIDTH) as usize
+    }
+
+    pub fn map_xy(&self, idx: usize) -> (i32, i32) {
+        (idx as i32 % SCREEN_WIDTH, idx as i32 / SCREEN_WIDTH)
+    }
+
+    pub fn render(&self, ctx: &mut BTerm) {
+        for (i, tile) in self.tiles.iter().enumerate() {
+            let (x, y) = self.map_xy(i);
+            match tile {
+                TileType::Wall => ctx.set(x, y, WHITE, BROWN1, to_cp437('#')),
+                TileType::Floor => ctx.set(x, y, WHEAT, BLACK, to_cp437('.')),
+            }
+        }
+    }
+}
--- artm/short-term-memo.md
+++ artm/short-term-memo.md
@@ -1,1 +1,11 @@
-animate Flappy
+map has a vec of tiles
+map_idx converts x y to index
+map's render renders tiles
+main
+    has state
+        contains a map
+        implements game state
+        renders map in tick
+    in main()
+        constructs ctx
+        starts main loop with ctx and state
