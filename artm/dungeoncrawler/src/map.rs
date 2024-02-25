4be682a2364669ba6c0b30648147b96edb704ffc
Artem Baguinski <abaguinski@gmail.com>
2024-02-25 09:45:56 +0000

    Implement BaseMap and Algorithm2D for Map

--- artm/dungeoncrawler/src/map.rs
+++ artm/dungeoncrawler/src/map.rs
@@ -19,8 +19,12 @@ impl Map {
         }
     }
 
-    pub fn map_idx(&self, pos: Point) -> usize {
+    pub fn point_idx(&self, pos: Point) -> usize {
         (pos.x + pos.y * WORLD_WIDTH) as usize
+    }
+
+    pub fn idx_point(&self, idx: usize) -> Point {
+        Point::new(idx % WORLD_WIDTH as usize, idx / WORLD_WIDTH as usize)
     }
 
     pub fn in_bounds(&self, pos: Point) -> bool {
@@ -28,14 +32,39 @@ impl Map {
     }
 
     pub fn can_enter(&self, pos: Point) -> bool {
-        self.in_bounds(pos) && self.tiles[self.map_idx(pos)] == TileType::Floor
+        self.in_bounds(pos) && self.tiles[self.point_idx(pos)] == TileType::Floor
     }
 
     pub fn try_idx(&self, pos: Point) -> Option<usize> {
         if self.in_bounds(pos) {
-            Some(self.map_idx(pos))
+            Some(self.point_idx(pos))
         } else {
             None
         }
+    }
+}
+
+impl BaseMap for Map {
+    fn get_available_exits(&self, idx: usize) -> SmallVec<[(usize, f32); 10]> {
+        let pos = self.idx_point(idx);
+        DIRS.iter()
+            .map(|delta| pos + *delta)
+            .filter(|dest| self.can_enter(*dest))
+            .map(|dest| (self.point_idx(dest), 1.0))
+            .collect()
+    }
+
+    fn get_pathing_distance(&self, idx1: usize, idx2: usize) -> f32 {
+        DistanceAlg::Pythagoras.distance2d(self.idx_point(idx1), self.idx_point(idx2))
+    }
+}
+
+impl Algorithm2D for Map {
+    fn dimensions(&self) -> Point {
+        Point::new(WORLD_WIDTH, WORLD_HEIGHT)
+    }
+
+    fn in_bounds(&self, pos: Point) -> bool {
+        self.in_bounds(pos)
     }
 }
